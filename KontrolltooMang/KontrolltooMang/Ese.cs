f366400d04ffffb55b01dff49239d08df03dc807
opilane <opilane@HP-CZC6297BB1>
2022-09-13 11:44:36 +0300

    1 kord

---
+++ KontrolltooMang/KontrolltooMang/Ese.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontrolltooMang
+{
+    abstract class Ese : IUksus //наследует от IUksus
+    {
+        int punktideArv;
+        string nimetus;
+
+        public Ese(string nimetus, int punktideArv)
+        {
+            this.nimetus = nimetus;
+            this.punktideArv = punktideArv;
+        }
+
+        public int PunktideArv()
+        {
+            return punktideArv;
+        }
+
+        public string info()
+        {
+            return nimetus;
+        }
+
+
+    }
+}
---
+++ KontrolltooMang/KontrolltooMang/Tegelane.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontrolltooMang
+{
+    abstract class Tegelane : IUksus //наследует от IUksus
+    {
+        List<Ese> eses;
+
+
+        string nimi;
+
+
+
+    }
+}
