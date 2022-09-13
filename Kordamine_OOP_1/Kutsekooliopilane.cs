5e0ebcdba7b541436cae2596af7a46107e06ee51
opilane <opilane@HP-CZC6297BB1>
2022-09-13 12:17:01 +0300

    asd

---
+++ Kordamine_OOP_1/Kutsekooliopilane.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kordamine_OOP_1
+{
+    class Kutsekooliopilane : Isik
+    {
+        public string oppeasutus;
+        public enum eriala { Welder, WebProgrammer }
+        public eriala Inimeriala;
+        public string kursus;
+        public int toetus;
+        public string elamiskoht;
+        public int lastearv;
+        public double perepalk;
+        public double keskhinne;
+        public double toetussumma;
+
+        double soidetoetus = 0;
+        double pohitoetus = 0;
+        double eritoetus = 0;
+        double tulek;
+
+        public Kutsekooliopilane(double toetussumma, string elamiskoht, double perepalk, int lastearv, double keskhinne, string oppeasutus, string eriala, string kursus, string nimi, int synniaasta, sugu Inimsugu, double maksuvaba, double palk, double tulumaks, eriala Inimeriala) : base(nimi, synniaasta, Inimsugu)
+        {
+            this.oppeasutus = oppeasutus;
+            this.Inimeriala = Inimeriala;
+            this.kursus = kursus;
+            this.toetus = toetus;
+            this.lastearv = lastearv;
+            this.perepalk = perepalk;
+            this.keskhinne = keskhinne;
+            this.toetussumma = toetussumma;
+
+        }
+
+        public string Toetus()
+        {
+            if (elamiskoht != "Tallinn")
+            {
+                soidetoetus = 50;
+            }
+            if (perepalk <= 300 || lastearv > 2)
+            {
+                eritoetus = 90;
+            }
+            if (keskhinne >= 3.3)
+            {
+                pohitoetus = 60;
+            }
+            string toetus = $"sõidetoetus {soidetoetus}, eritoetus {eritoetus}, põhitoetus {pohitoetus}";
+            return toetus;
+        }
+        public override double arvutaSissetulek()
+        {
+            tulek = toetussumma + soidetoetus + eritoetus + pohitoetus;
+            return tulek;
+        }
+
+
+        public override void printInfo()
+        {
+            Console.WriteLine("Sinu toetus on {0} õppeasutus on {1}, eriala {2}, kursus {3} ja tootasu {4}.", toetus, oppeasutus, Inimeriala, kursus);
+        }
+
+    }
+}
---
+++ Kordamine_OOP_1/Opilane.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kordamine_OOP_1
+{
+    class Opilane : Isik
+    {
+        public string koolinimi;
+        public string klass;
+        public string spetsialiseerumine;
+
+        public Opilane(string koolinimi, string klass, string spetsialiseerumine, string nimi, int synniaasta, sugu InimSugu, double maksuvaba, double palk) :base(nimi, synniaasta, InimSugu)
+        {
+            this.koolinimi = koolinimi;
+            this.klass = klass;
+            this.spetsialiseerumine = spetsialiseerumine;
+        }
+        public override double arvutaSissetulek()
+        {
+            double netopalk = ((palk - maksuvaba) * (1 - (tulumaks / 100))) + maksuvaba;
+            return netopalk;
+        }
+
+        public override void printInfo()
+        {
+            Console.WriteLine("Tema koolinimi on {0} ja klass {1} ja eriala {2}",koolinimi ,klass, spetsialiseerumine);
+        }
+
+
+    }
+}   
