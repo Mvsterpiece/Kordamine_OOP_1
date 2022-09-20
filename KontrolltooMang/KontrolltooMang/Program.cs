ca4d4424832892caf3b1bf6328f44605222afdb0
opilane <opilane@HP-CZC6297BB1>
2022-09-20 11:33:49 +0300

    asd

--- KontrolltooMang/KontrolltooMang/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using KontrolltooMang;
-using System.Runtime.InteropServices;
-
-Ese ese = new Ese("Bob",15);
-Tegelane tegelane = new Tegelane("Bebra");
-ese.info();
-tegelane.info();
---
+++ WindowsForms_TARpv21/WindowsForms_TARpv21/MinuOmaVorm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_TARpv21
+{
+    public partial class MinuOmaVorm : Form
+    {
+        TreeView puu;
+        Button nupp;
+        Label silt;
+        public MinuOmaVorm()
+        {
+            Height = 600;
+            Width = 900;
+            Text = "Minu oma vorm koos elementidega"; //название формы
+            puu= new TreeView();
+            puu.Dock = DockStyle.Left;
+            puu.Location = new Point(0,0);
+            TreeNode oksad= new TreeNode("Elemendid");
+            oksad.Nodes.Add(new TreeNode("Nupp-Button"));
+            oksad.Nodes.Add(new TreeNode("Silt-Label"));
+            oksad.Nodes.Add(new TreeNode("Dialog aken - MessageBox"));
+
+            puu.AfterSelect += Puu_AfterSelect;
+
+
+            puu.Nodes.Add(oksad);
+            this.Controls.Add(puu);
+
+
+            
+            BackColor = Color.White;
+
+        }
+
+        private void Puu_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text == "Nupp-Button")
+            {
+                nupp = new Button();
+                nupp.Text = "Vajuta siia";
+                nupp.Height = 30;
+                nupp.Width = 100;
+                nupp.Location = new Point(200, 100);
+                nupp.Click += Nupp_Click;
+                this.Controls.Add(nupp);
+            }
+            else if (e.Node.Text == "Silt-Label")
+            {
+                silt = new Label
+                {
+                    Text = "Minu esimene vorm",
+                    Font= new Font("ArialBlack",10),
+                    Size = new Size(200, 50),
+                    Location = new Point(450, 0)
+                };
+                silt.MouseEnter += Silt_MouseEnter;
+                silt.MouseLeave += Silt_MouseLeave;
+                this.Controls.Add(silt);
+            }
+            else if (e.Node.Text == "Dialog aken - MessageBox")
+            {
+                MessageBox.Show("Aken", "kõige lihtne aken");
+                var vastus = MessageBox.Show("kas paneme aken kinni?","Valik",
+                    MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if(vastus==DialogResult.Yes)
+                {
+                    this.Close();
+                }
+                else { }
+            }
+
+        }
+        private void Silt_MouseEnter(object sender, EventArgs e)
+        {
+            silt.ForeColor = Color.White;
+            silt.BackColor = Color.Black;
+        }
+
+        private void Silt_MouseLeave(object sender, EventArgs e)
+        {
+            silt.ForeColor = Color.Black;
+            silt.BackColor = Color.Transparent;
+        }
+
+        Random random = new Random();
+        private void Nupp_Click(object sender, EventArgs e)
+        {
+            int red, green, blue;
+            red = random.Next(255);
+            green = random.Next(255);
+            blue= random.Next(255);
+
+            this.BackColor = Color.FromArgb(red,green,blue);
+        }
+
+    }
+}
